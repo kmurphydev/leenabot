32bb8c7f42752d60de2d2300fc8851228a22785d
Keenan Murphy <kmurphydev@gmail.com>
2022-01-05 13:30:40 -0500

    built discord side of remindme feature (message embed + buttons, interaction collector + event handlers

--- commands/remindme.commands.js
+++ commands/remindme.commands.js
@@ -1,4 +1,6 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const Reminder = require('../entity/Reminder');
 // const { MessageActionRow, MessageSelectMenu } = require('discord.js');
 
 module.exports = {
@@ -10,17 +12,21 @@ module.exports = {
                 .setDescription('how long to wait until you are reminded')
                 .setRequired(true)
         )
-        .addStringOption(option =>
+        .addNumberOption(option =>
             option.setName('duration_units')
                 .setDescription('units of time for your duration')
                 .setChoices([
                     [
                         'minutes',
-                        'minutes'
+                        60 * 1000
                     ],
                     [
                         'hours',
-                        'hours'
+                        60 * 60 * 1000
+                    ],
+                    [
+                        'days',
+                        24 * 60 * 60 * 1000
                     ]
                 ])
                 .setRequired(true)
@@ -31,13 +37,79 @@ module.exports = {
                 .setRequired(true))
     ,
     async execute(interaction) {
-        interaction.reply('placeholder smile :))');
+        const reminder_text = interaction.options.getString('reminder_message');
+        const reminder_time = interaction.options.getNumber('duration') * interaction.options.getNumber('duration_units');
 
-        //prompt for reminder message
+        const reminder_date = new Date(Date.now() + reminder_time);
+        const date_options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+        const time_string = reminder_date.toLocaleTimeString('en-US');
+        const date_string = reminder_date.toLocaleDateString('en-US', date_options);
+        const datetime_string = time_string + ' on ' + date_string;
 
-        //prompt for reminder timeOut
 
-        //show an embed with reminder details and ask to confirm
+        const exampleEmbed = new MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(reminder_text)
+            .addFields(
+                { name: '\u200B', value: '\u200B' },
+            )
+            .setDescription('will be sent to you at the following date and time')
+            .addFields(
+                { name: 'Time', value: time_string, inline: true },
+                { name: 'Date', value: date_string, inline: true },
+            );
+
+        const buttonRow = new MessageActionRow()
+            .addComponents(
+                new MessageButton()
+                    .setCustomId('confirm_reminder')
+                    .setLabel('Set Reminder')
+                    .setStyle('SUCCESS')
+                ,
+                new MessageButton()
+                    .setCustomId('cancel_reminder')
+                    .setLabel('Cancel')
+                    .setStyle('DANGER')
+            )
+
+        // const message_text = 'your reminder text is ' + reminder_text + '.'
+        //     + 'your reminder will be triggered in ' + reminder_time + 'ms,'
+        //     + ' at ' + datetime_string + '.'
+
+        interaction.reply({
+            // content: message_text,
+            embeds: [exampleEmbed],
+            ephemeral: true,
+            components: [buttonRow]
+        })
+            .then(() => {
+                const filter = i => i.user.id === interaction.user.id;
+
+                const collector = interaction.channel.createMessageComponentCollector({ filter, max: 1, time: 15000 });
+
+                let selectedFlag = false;
+
+                collector.on('collect', async i => {
+                    if (i.customId === 'confirm_reminder') {
+                        selectedFlag = true;
+                        await interaction.editReply({ components: [] });
+                        await interaction.followUp({ content: 'Reminder set! See you soon.', ephemeral: true });
+                    } else if (i.customId === 'cancel_reminder') {
+                        selectedFlag = true;
+                        await interaction.editReply({ components: [] });
+                        await interaction.followUp({ content: 'Reminder cancelled.', ephemeral: true });
+                    }
+                })
+
+                collector.on('end', async collected => {
+                    if (!selectedFlag) {
+                        await interaction.editReply({ components: [] })
+                        await interaction.followUp({ content: 'Nothing was selected. Reminder cancelled by timeout', ephemeral: true });
+                    }
+                })
+
+            })
+            ;
 
     },
 };
