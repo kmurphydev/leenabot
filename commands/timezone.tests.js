9be1bff7220bc13ba894d07a27c940e793d45f86
Keenan Murphy <kmurphydev@gmail.com>
2022-02-04 12:36:45 -0500

    refactored timezone.tests to break into smaller tests and group tests using describe() blocks

--- commands/timezone.tests.js
+++ commands/timezone.tests.js
@@ -31,7 +31,8 @@ jest.mock('../model/User', () => {
     return { User: User };
 });
 
-test('offset subcommand', async () => {
+describe('offset subcommand', () => {
+
     const generateTestInteraction = (offsetOption, discord_id, replyContent) => {
 
         const testInteraction = {
@@ -62,69 +63,99 @@ test('offset subcommand', async () => {
     };
 
 
-    const interaction1 = generateTestInteraction(
-        'UTC-1000',
-        0,
-        'Your timezone has been set to (UTC-10:00)'
-    )
-    await execute(interaction1);
+    test('UTC-1000', async () => {
+        const interaction1 = generateTestInteraction(
+            'UTC-1000',
+            0,
+            'Your timezone has been set to (UTC-10:00)'
+        )
+        await execute(interaction1);
 
-    const interaction2 = generateTestInteraction(
-        'babu frik',
-        0,
-        'asdf'
-    );
-    await expect(execute(interaction2)).rejects.toThrow(
-        'You did not enter a recognized format for offset: examples of valid offsets include -1200, UTC+0830, +0830, UTC-0400, +945, 945'
-    );
+    })
+    test('bad format (arbitrary string)', async () => {
 
-    const interaction3 = generateTestInteraction(
-        '-1500',
-        0,
-        'asdf'
-    );
-    await expect(execute(interaction3)).rejects.toThrow(
-        'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
-    )
-    const interaction4 = generateTestInteraction(
-        '-15:00',
-        0,
-        'asdf'
-    );
-    await expect(execute(interaction4)).rejects.toThrow(
-        'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
-    )
-    const interaction5 = generateTestInteraction(
-        'utC-15:00',
-        0,
-        'asdf'
-    );
-    await expect(execute(interaction5)).rejects.toThrow(
-        'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
-    )
-    const interaction6 = generateTestInteraction(
-        '-10:01',
-        0,
-        'asdf'
-    );
-    await expect(execute(interaction6)).rejects.toThrow(
-        'You did not enter an offset corresponding to a valid timezone'
-    )
-    const interaction7 = generateTestInteraction(
-        'UTC+3:00',
-        0,
-        'Your timezone has been set to (UTC+03:00)'
-    );
-    await execute(interaction7);
-    const interaction8 = generateTestInteraction(
-        'UTC+3:00',
-        1,
-        'Your timezone has been set to (UTC+03:00)'
-    );
-    await execute(interaction8);
+        const interaction2 = generateTestInteraction(
+            'babu frik',
+            0,
+            'asdf'
+        );
+        await expect(execute(interaction2)).rejects.toThrow(
+            'You did not enter a recognized format for offset: examples of valid offsets include -1200, UTC+0830, +0830, UTC-0400, +945, 945'
+        );
+
+    })
+
+    test('bad value (-1500 < -1200)', async () => {
+
+        const interaction3 = generateTestInteraction(
+            '-1500',
+            0,
+            'asdf'
+        );
+        await expect(execute(interaction3)).rejects.toThrow(
+            'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
+        )
+    })
+
+    test('bad value (-15:00 < -1200)', async () => {
+
+        const interaction4 = generateTestInteraction(
+            '-15:00',
+            0,
+            'asdf'
+        );
+        await expect(execute(interaction4)).rejects.toThrow(
+            'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
+        )
+    })
+
+    test('bad value (utc-15:00 < -1200)', async () => {
+
+        const interaction5 = generateTestInteraction(
+            'utC-15:00',
+            0,
+            'asdf'
+        );
+        await expect(execute(interaction5)).rejects.toThrow(
+            'You did not enter a valid timezone offset value. Offset must be between -1200 and +1400'
+        )
+    })
+
+    test('invalid timezone (-10:01)', async () => {
+
+        const interaction6 = generateTestInteraction(
+            '-10:01',
+            0,
+            'asdf'
+        );
+        await expect(execute(interaction6)).rejects.toThrow(
+            'You did not enter an offset corresponding to a valid timezone'
+        )
+    })
+
+    test('UTC+3', async () => {
+
+        const interaction7 = generateTestInteraction(
+            'UTC+3:00',
+            0,
+            'Your timezone has been set to (UTC+03:00)'
+        );
+        await execute(interaction7);
+    })
+    test('UTC-3', async () => {
+
+        const interaction8 = generateTestInteraction(
+            'UTC-3:00',
+            1,
+            'Your timezone has been set to (UTC-03:00)'
+        );
+        await execute(interaction8);
+    })
 
 })
-test('abbreviation command', async () => {
+
+describe('abbreviation subcommand', () => {
+
     const generateTestInteraction = (abbrevOption, discord_id, replyContent) => {
 
         const testInteraction = {
@@ -156,41 +187,73 @@ test('abbreviation command', async () => {
 
 
 
-    const interaction1 = generateTestInteraction(
-        'babu frik',
-        0,
-        'asdf'
-    );
-    expect(execute(interaction1)).rejects.toThrow(
-        'You did not enter a valid timezone abbreviation'
-    );
+    test('invalid abbreviation (arbitrary string)', async () => {
+
+        const interaction1 = generateTestInteraction(
+            'babu frik',
+            0,
+            'asdf'
+        );
+        expect(execute(interaction1)).rejects.toThrow(
+            'You did not enter a valid timezone abbreviation'
+        );
+
+    })
+
+    test('EST', async () => {
+
+        await execute(generateTestInteraction(
+            'est',
+            0,
+            'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
+        ));
+    })
 
 
-    await execute(generateTestInteraction(
-        'est',
-        0,
-        'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
-    ));
-    await execute(generateTestInteraction(
-        'eST',
-        0,
-        'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
-    ));
-    await expect(execute(generateTestInteraction(
-        'eST1',
-        0,
-        'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
-    ))).rejects.toThrow('You did not enter a valid timezone abbreviation');
-    await execute(generateTestInteraction(
-        'EDT',
-        0,
-        'Your timezone has been set to (UTC-04:00) Eastern Daylight Time (US & Canada)'
-    ));
-    await execute(generateTestInteraction(
-        'SPST',
-        1,
-        'Your timezone has been set to (UTC-05:00) Bogota, Lima, Quito'
-    ));
+    test('eST', async () => {
+
+        await execute(generateTestInteraction(
+            'eST',
+            0,
+            'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
+        ));
+    })
+
+    test('eST1 should be invalid', async () => {
+
+        await expect(execute(generateTestInteraction(
+            'eST1',
+            0,
+            'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
+        ))).rejects.toThrow('You did not enter a valid timezone abbreviation');
+    })
+
+    test('1eST should be invalid', async () => {
+
+        await expect(execute(generateTestInteraction(
+            '1eST',
+            0,
+            'Your timezone has been set to (UTC-05:00) Eastern Time (US & Canada)'
+        ))).rejects.toThrow('You did not enter a valid timezone abbreviation');
+    })
+
+    test('EDT', async () => {
+
+        await execute(generateTestInteraction(
+            'EDT',
+            0,
+            'Your timezone has been set to (UTC-04:00) Eastern Daylight Time (US & Canada)'
+        ));
+    })
+
+    test('SPST', async () => {
+
+        await execute(generateTestInteraction(
+            'SPST',
+            1,
+            'Your timezone has been set to (UTC-05:00) Bogota, Lima, Quito'
+        ));
+    })
 
 
 
